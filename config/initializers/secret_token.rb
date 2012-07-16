22d403740578bf3515c9c3dc2261f34b71611dbb
Eric MacAdie <Eric@MacAdie.net>
2012-07-16 14:13:15 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuPinger::Application.config.secret_token = 'ddc667edf1aa5e6adfcd2a4998ec6e92dc89beafca1c1ba407533f1d16c42e74e67180b15882e3966c0926ed756fda1694368d868f84c111cd2a6218327e55eb'
