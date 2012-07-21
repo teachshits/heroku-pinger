b59da737aa11bf679868abb9e8f19b55dca254ff
Eric MacAdie <Eric@MacAdie.net>
2012-07-21 02:11:35 -0500

    Adding tests for website model

--- app/models/website.rb
+++ app/models/website.rb
@@ -9,6 +9,7 @@ class Website < ActiveRecord::Base
     :message => "%{value} is not a valid value, must be one of 0, 1, 2, 3, 4 or 5" }
   validates :minute, :presence => true
   validates :user_id, :presence => true
+  validates :url, :presence => true
   validates_format_of :url, :with => /^(http):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
 
   belongs_to :user
--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -2,8 +2,50 @@ require 'spec_helper'
 
 describe Website do
 
+=begin
   before :each do 
-    
+    @website = Website.new(
+      minute: 1, user_id: 1, url: "http://www.yahoo.com"
+    )
   end
+=end
+  before (:each) do
+    # this works:
+    @website = FactoryGirl.create(:website)
+    # this also works:
+    # @user = User.new( :provider => "twitter", :uid => "12345", :name => "the user")
+    @website.save
+  end
+  let(:website) {
+    Website.new(
+      minute: 1, user_id: 1, url: "http://www.yahoo.com"
+    )
+  }
+
+  subject { @website }
+
+  it { should respond_to(:failed_tries) }
+  it { should respond_to(:minute) }
+  it { should respond_to(:name) }
+  it { should respond_to(:successful_tries) }
+  it { should respond_to(:summary) }
+  it { should respond_to(:url) }
+  it { should respond_to(:user_id) }
+
+  describe "fails with missing user id" do
+    before { @website.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "fails with missing minute" do
+    before { @website.minute = nil }
+    # it { should_not be_valid }
+  end
+
+  describe "fails with missing url" do
+    before { @website.url = nil }
+    it { should_not be_valid }
+  end
+
 
 end
