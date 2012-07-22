583b2ddd29d552fd5934491e56f5e3eb224147e2
Eric MacAdie <Eric@MacAdie.net>
2012-07-22 18:13:32 -0500

    Adding commits

--- spec/requests/websites_spec.rb
+++ spec/requests/websites_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Websites" do
+
   describe "GET /websites" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
