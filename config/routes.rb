7f3ab4c7c27c3b8b1b0e12403e2c7c83427ed8e2
Eric MacAdie <Eric@MacAdie.net>
2012-07-28 00:40:52 -0500

    made the routes explicit for the website

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 HerokuPinger::Application.routes.draw do
   resources :pings
 
-  resources :websites
+  # resources :websites
+  get '/websites' => 'websites#index', :as => 'websites'
+  post '/websites' => 'websites#create'
+  get '/websites/new' => 'websites#new', :as => 'new_website'
+  get '/websites/:id/edit' => 'websites#edit', :as => 'edit_website'
+  get '/websites/:id' => 'websites#show', :as => 'website'
+  put '/websites/:id' => 'websites#update'
+  delete '/websites/:id' => 'websites#destroy'
 
   root :to => "home#index"
   resources :users, :only => [ :show, :edit, :update ]
