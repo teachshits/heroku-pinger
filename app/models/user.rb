149ec16fe98d9a81789ed7fbeb16213f6973962d
Eric MacAdie <Eric@MacAdie.net>
2012-09-06 20:36:36 -0500

    Still working on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       user.number_of_sites = 0
-      user.is_admin=false
+      user.set_not_admin
       user.provider = auth['provider']
       user.uid = auth['uid']
       if auth['info']
@@ -13,15 +13,24 @@ class User < ActiveRecord::Base
       end
     end
   end
- 
+
+ def set_not_admin
+   logger.error "Calling set_not_admin"
+   self.is_admin = false
+   logger.error "leaving set_not_admin"
+ end
+
+=begin 
   def is_admin=(bool_arg)
+    logger.error "calling is_admin="
     if bool_arg == true
       self.is_admin = true
     else
       self.is_admin = false
     end
+    logger.error "Done with is_admin="
   end
-
+=end
   validates :name, :presence => true
   validates :uid, :presence => true
   validates :number_of_sites, :presence => true
