cd40d73279dbe5436e55de8abd86b12f956520b9
Eric MacAdie <Eric@MacAdie.net>
2013-01-19 09:46:55 -0600

    Improved the tests

--- spec/requests/websites_spec.rb
+++ spec/requests/websites_spec.rb
@@ -56,7 +56,7 @@ describe "Websites" do
       # puts "------ get method: #{self.method(:get).owner} "
       # puts "------ response.class: #{response.class}"
       # puts "------ response.body: #{response.body}"
-      # response.body.should have_content('Listing websites')
+      response.body.should have_content('Listing websites')
       response.body.should_not have_content('New Website') 
     end
 
@@ -64,40 +64,16 @@ describe "Websites" do
       # not valid here for some reason
       # session[:user_id] = user.id
       p = SessionProvider.get_session
-      puts "p is a #{p.class}"
-      puts "Inspecting the session: #{p.to_s}"
-      puts "Here is user.id: #{user.id}"
+      # puts "p is a #{p.class}"
+      # puts "Inspecting the session: #{p.to_s}"
+      # puts "Here is user.id: #{user.id}"
       visit "/websites"
       # get websites_path, {}, valid_session_from_user(user.id)
       # get websites_path, {}, valid_session #_from_user(user.id)
-      puts "------ get method: #{self.method(:get).owner} "
-      puts "------ response.class: #{response.class}"
-      puts "response.body: #{response.body}"
+      # puts "------ get method: #{self.method(:get).owner} "
+      # puts "------ response.class: #{response.class}"
+      # puts "response.body: #{response.body}"
       response.body.should have_content('Listing websites')
-      response.body.should have_content('New Website') 
-    end
-
-    xit "should contain a link to create a website when you are logged in" do
-
-      # mock_omni_auth
-=begin
-      OmniAuth.config.mock_auth[:twitter] = {
-        'uid' => '12345',
-        'provider' => 'twitter',
-        'info' => {
-          'name' => 'Jimmy'
-        }
-      } 
-=end
-      # OmniAuth.config.add_mock(:twitter, {:uid => '12345'})
-
-      get websites_path, {}, valid_session
-      # puts "-----------------------------"
-      puts "response.body: #{response.body}"
-      puts "-----------------------------"
-      puts "valid_attributes: #{valid_attributes.to_s}"
-      puts "valid_session: #{valid_session.to_s}"
-      
       response.body.should have_content('New Website') 
     end
 
